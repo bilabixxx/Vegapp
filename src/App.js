ecc47b765d309d3b212318ea4cd0bc1d2b8358fd
bilabixxx <gennuso.biagio@gmail.com>
2022-05-19 17:08:52 +0200

    Updated import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home'
 import { Detail } from './pages/Detail/Detail';
