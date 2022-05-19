380ae2b1893203fe95dfa02c6a075a0701bdf2ff
bilabixxx <gennuso.biagio@gmail.com>
2022-05-19 17:27:02 +0200

    Updated header components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className='header p-4 m-auto d-flex align-items-center'>
-      <p className='logo'>VEG<span className='big'>A</span><span>PP</span></p>
+     <Link className="logo-image" to="/"><p className='logo'>VEG<span className='big'>A</span><span>PP</span></p></Link> 
       <Link className="ms-5 h3 menu-item" to="/">HOME</Link>
     </div>
   )
